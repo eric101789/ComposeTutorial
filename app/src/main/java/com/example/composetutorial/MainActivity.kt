6ec6744ca61391236b0429b52a881b87edadbbdb
eric101789 <eric10179@gmail.com>
2023-03-27 18:16:11 +0800

    版面配置 使用質感設計(形狀)

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -39,27 +40,30 @@ fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Content profile picture",
+            contentDescription = null,
             modifier = Modifier
-                // Set image size to 40 dp
                 .size(40.dp)
-                // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-
-        // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
 
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = msg.body,
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.body2
+            )
         }
     }
-
-
 }
 
 @Preview
